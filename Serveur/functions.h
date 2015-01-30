f45e00c33b1c9b7f71b4c4f578db47145e7d7e95
leonbarron.cedric@gmail.com <leonbarron.cedric@gmail.com@cb36ddf7-eee8-3b3c-a8b0-bd962b50de96>
2015-01-30 18:36:58 +0000

    base du serveur

--- Serveur/functions.h
+++ Serveur/functions.h
@@ -1,50 +1,16 @@
-#ifndef ECHANGE_TCP_H
-#define ECHANGE_TCP_H
-
-// Libs
-#include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <termios.h>
-#include <sys/fcntl.h>
-#include <pthread.h>
-#include <errno.h>
-#include <signal.h>
-
-// CanBus
-#include <net/if.h>
-#include <sys/ioctl.h>
-#include <linux/can.h>
-#include <linux/can/raw.h>
-
-// Constantes
-#define PORT 8080
-#define LG_MESSAGE 256
-#define CLIENT_MAX 5
-#define TAILLE_TRAME 10
-#define TAILLE_TRAME_CAN 8
-#define TAILLE_INFO_TRAME 3
-#define TAILLE_INFO_TRAME_CAN 1
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
 
 
-int initLiaisonSerie();
-int initLiaisonCan();
+#define CLIENT_MAX 1
+typedef enum {GAUCHE, DROITE} Orientation;
 
-ssize_t lectureTrame(int liaisonSerie, char * buffer,  size_t nbMax);
-ssize_t lectureTrameCan(int liaisonCan, char *buffer, size_t tailleBuffer);
 
-int saveTrame(FILE* fptr, char *buffer, int sizeofbuffer);
-int saveTrameCan(FILE* fptr, char *bufferCan, int sizeofbuffercan);
+void avancer();
+void reculer();
+void tourner(Orientation direction);
+void arretUrgence(); // si la file de commande est rempli on arrete le systeme
 
-int numberOfEncodingDigits(int number);
-void convertIntToChar(int value, char* result, int resultSize);
-
-void concatenation(char* frameSerie, char* frameCan,char* tailleTrameSerieLue_encode, char* tailleTrameCanLue_encode);
 
 #endif
+
---
+++ Serveur/server.c
@@ -0,0 +1,126 @@
+// Fonctions
+#include "functions.h"
+#include <sys/socket.h>
+
+typedef enum {GAUCHE, DROITE} Orientation;
+
+
+void main()
+{
+	
+    int socketServeur;
+    int socketClient;
+	int clientConnecte = 0;
+    int i;
+	int lu = 0;
+    struct sockaddr_in addrServeur;
+    socklen_t longueurAdresse; // Nombre d'octets de la structure sockaddr_in
+    char nomDuClient[1024], portDuClient[32];
+	char commande;
+
+    // Crée un socket de communication
+	socketServeur = socket(PF_INET, SOCK_STREAM, 0);
+    if(socketServeur == -1)
+    {
+        perror("Socket");
+        exit(-1);
+    }
+
+    printf("Socket crée avec succès ! (%d)\n", socketServeur);
+
+    addrServeur.sin_addr.s_addr = INADDR_ANY; //toutes
+    addrServeur.sin_family = PF_INET;
+    addrServeur.sin_port = htons(PORT);
+	
+	// lose the pesky "Address already in use" error message
+	int yes = 1;
+	if( setsockopt(socketServeur, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1 ) 
+	{
+		perror("setsockopt");
+		exit(errno);
+	}
+
+    // Demande l'attachement local de la socket
+    longueurAdresse = sizeof(addrServeur);
+    if( bind(socketServeur, (struct sockaddr *)&addrServeur, longueurAdresse) == -1 )
+    {
+        perror("bind");
+        exit(-2);
+    }
+    printf("Socket attachée avec succès!\n");
+
+    if (listen(socketServeur, CLIENT_MAX) == -1)
+    {
+        perror("listen");
+        exit(errno);
+    }
+    printf("Socket placée en écoute passive...\n");
+
+	printf("Attente d'une demande de connexion (quitter avec Cltrl-C)\n\n");
+		
+    while(1)
+    {
+       
+		if(clientConnecte == 0)
+		{
+			socketClient = accept(socketServeur, (struct sockaddr *)&addrServeur, &longueurAdresse);
+			
+			if(socketClient == -1 )
+			{
+				perror("accept");
+				close(socketClient);
+				close(socketServeur);
+				exit(errno);		
+			}
+			
+			printf("Nouveau client !\n");
+
+			if ( getnameinfo((struct sockaddr*)&addrServeur, sizeof(addrServeur), nomDuClient, sizeof(nomDuClient), portDuClient,
+						   sizeof(portDuClient), NI_NUMERICHOST | NI_NUMERICSERV) == 0)
+			{
+				   printf("client=%s, port=%s\n", nomDuClient, portDuClient);
+			}
+			
+			clientConnecte = 1;
+		}
+		
+		lu = read(socketClient,  &commande, sizeof char);
+		
+		
+		if(lu == -1)
+		{
+			printf("Error reading from socketClient ! \n");
+            printf("errno = %d \n", errno);
+			close(socketClient);
+			clientConnecte = 0;
+			continue;
+		}
+		
+		if(lu != 1)
+		{
+			printf("Nombre d'octets lu incorrect\n");
+		}
+		
+		switch commande
+		{
+			case 'a' :
+				avancer();
+				break;
+			case 'r' :
+				reculer();
+				break;
+			case 'd' :
+				tourner(DROITE);
+				break;
+			case 'g' :
+				tourner(GAUCHE);
+				break;
+		}
+	   
+    }
+	
+	
+	
+	
+	
+}
