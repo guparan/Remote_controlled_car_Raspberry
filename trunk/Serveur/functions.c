227fc4f3ce9de70b20df5e86bb17eb43d869a2ee
leonbarron.cedric@gmail.com <leonbarron.cedric@gmail.com@cb36ddf7-eee8-3b3c-a8b0-bd962b50de96>
2015-02-08 10:20:07 +0000

    création et des des fonctions avancer, reculer, et tourner

--- trunk/Serveur/functions.c
+++ trunk/Serveur/functions.c
@@ -2,6 +2,10 @@
 
 void initGPIO()
 {
+	printf("Initialisation des GPIO\n");
+
+	if(wiringPiSetup() == -1) return 1;
+	
 	pinMode(PIN23, OUTPUT);
 	pinMode(PIN24, OUTPUT);
 	pinMode(PIN8, OUTPUT);
@@ -10,10 +14,6 @@ void initGPIO()
 
 void avancer()
 {
-	pinMode(PIN23, OUTPUT);
-	pinMode(PIN24, OUTPUT);
-	pinMode(PIN8, OUTPUT);
-	pinMode(PIN7, OUTPUT);
 	digitalWrite (PIN24, 1);     // On
     digitalWrite (PIN8, 0);      // Off
     digitalWrite (PIN7, 1);      // On
@@ -22,10 +22,31 @@ void avancer()
 
 void reculer()
 {
-
+	digitalWrite (PIN24, 0);     // On
+    digitalWrite (PIN8, 1);      // Off
+    digitalWrite (PIN7, 0);      // On
+    digitalWrite (PIN23, 1);     // Off
 }
 
 void tourner(Direction d)
 {
+	if(d == GAUCHE)
+	{
+	      digitalWrite (PIN24, 0);     // Off
+   	      digitalWrite (PIN8, 0);      // Off
+   	      digitalWrite (PIN7, 1);      // On
+   	      digitalWrite (PIN23, 1);     // On
+	     printf("Je tourne a gauche \n");
+
+	}
+	else 
+	{
+	      digitalWrite (PIN24, 1);     // On
+	      digitalWrite (PIN8, 1);      // On
+              digitalWrite (PIN7, 0);      // Off
+   	      digitalWrite (PIN23, 0);     // Off
+	      printf("Je tourne a droite \n");
+
+	}
 
 }
