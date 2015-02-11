331f611094e31618d6aa54390bad63df87bdef21
leonbarron.cedric@gmail.com <leonbarron.cedric@gmail.com@cb36ddf7-eee8-3b3c-a8b0-bd962b50de96>
2015-02-11 16:28:01 +0000

    MAJ client

--- Client/ClientSocketUDP_Android2/src/com/example/clientsocketudp_android2/MainActivity.java
+++ Client/ClientSocketUDP_Android2/src/com/example/clientsocketudp_android2/MainActivity.java
@@ -1,16 +1,11 @@
 package com.example.clientsocketudp_android2;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-import android.R.string;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
@@ -26,14 +21,8 @@ public class MainActivity extends Activity {
     EditText editTextAddress, editTextPort;
     Button buttonConnect, buttonClear, buttonOK;
     Socket socket;
-    DataInputStream input;
-    BufferedReader in;
-    //String message_distant;
-    char[] message_distant = new char[1024];
-    int tailleTrameSerie;
-    int tailleTrameCan;
-    String trameSerie;
-    String trameCan;
+    //BufferedReader in;
+   BufferedWriter out;
     
     
 
@@ -93,7 +82,8 @@ public class MainActivity extends Activity {
         	try 
         	{
         		socket = new Socket(editTextAddress.getText().toString(), Integer.parseInt(editTextPort.getText().toString()) );
-        		in = new BufferedReader (new InputStreamReader (socket.getInputStream()));
+        		//in = new BufferedReader (new InputStreamReader (socket.getInputStream()));
+        		out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
             } catch (UnknownHostException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
@@ -104,42 +94,20 @@ public class MainActivity extends Activity {
     		
     		while(true)
     		{
-		        try {
-					int nblus = in.read(message_distant, 0, 1024);
-	              //  textPerformance.setText( String.valueOf(message_distant) );
+    			try {
+					out.write('a');
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				}		            
-		        
-
-		        
-		        tailleTrameSerie = Integer.parseInt(String.copyValueOf(message_distant, 0, 3));
-		        tailleTrameCan = Integer.parseInt(String.copyValueOf(message_distant, 3, 1));
-		        trameSerie = String.copyValueOf(message_distant, 4, tailleTrameSerie);
-		       // trameCan = String.copyValueOf(message_distant, 4+tailleTrameSerie, tailleTrameCan);
-		        trameCan = asciiValuesOf(message_distant, 4+tailleTrameSerie, tailleTrameCan);
-		 
-		        textPerformance.setText(trameSerie+trameCan);
-		        
-		        
-               // int bytesRead;
-               // while ((bytesRead = inputStream.read(buffer)) != -1){
-                 //   byteArrayOutputStream.write(buffer, 0, bytesRead); 
-                //}   
-                
-                // TODO gérer le close
-                //socket.close();                ,
-		        //in.close();
-
-                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);               
-                
+				}
+					              
                 try {
-					response = byteArrayOutputStream.toString("UTF-8");
-				} catch (UnsupportedEncodingException e) {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+                
     		}
         }
         
