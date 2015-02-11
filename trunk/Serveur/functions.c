66d680ecf4aceb93552781f1b6e703826d234246
guparan@gmail.com <guparan@gmail.com@cb36ddf7-eee8-3b3c-a8b0-bd962b50de96>
2015-02-11 16:19:18 +0000

    Corrections erreurs serveur

--- trunk/Serveur/functions.c
+++ trunk/Serveur/functions.c
@@ -1,19 +1,21 @@
 #include "functions.h"
 
+#include <stdio.h>
+
 void initGPIO()
 {
 	printf("Initialisation des GPIO\n");
 
-	if(wiringPiSetup() == -1) return 1;
+	if(wiringPiSetup() == -1) return;
 	
 	//pinMode(PIN23, OUTPUT);
 	//pinMode(PIN24, OUTPUT);
 	//pinMode(PIN8, OUTPUT);
 	//pinMode(PIN7, OUTPUT);
 	softPwmCreate(PIN23,0,1000);
-        softPwmCreate(PIN24,0,1000);
-        softPwmCreate(PIN8,0,1000);
-        softPwmCreate(PIN7,0,1000);
+	softPwmCreate(PIN24,0,1000);
+	softPwmCreate(PIN8,0,1000);
+	softPwmCreate(PIN7,0,1000);
 }
 
 void avancer()
@@ -25,8 +27,7 @@ void avancer()
    softPwmWrite(PIN24,500);
    softPwmWrite(PIN8,0);
    softPwmWrite(PIN7,500);
-   softPwmWrite(PIN23,0);
-  
+   softPwmWrite(PIN23,0);  
 }
 
 void reculer()
@@ -41,21 +42,19 @@ void tourner(Direction d)
 {
 	if(d == GAUCHE)
 	{
-	      digitalWrite (PIN24, 0);     // Off
-   	      digitalWrite (PIN8, 0);      // Off
-   	      digitalWrite (PIN7, 1);      // On
-   	      digitalWrite (PIN23, 1);     // On
-	     printf("Je tourne a gauche \n");
+		digitalWrite (PIN24, 0);     // Off
+		digitalWrite (PIN8, 0);      // Off
+		digitalWrite (PIN7, 1);      // On
+		digitalWrite (PIN23, 1);     // On
+		printf("Je tourne a gauche \n");
 
 	}
 	else 
 	{
-	      digitalWrite (PIN24, 1);     // On
-	      digitalWrite (PIN8, 1);      // On
-              digitalWrite (PIN7, 0);      // Off
-   	      digitalWrite (PIN23, 0);     // Off
-	      printf("Je tourne a droite \n");
-
+		digitalWrite (PIN24, 1);     // On
+		digitalWrite (PIN8, 1);      // On
+		digitalWrite (PIN7, 0);      // Off
+		digitalWrite (PIN23, 0);     // Off
+		printf("Je tourne a droite \n");
 	}
-
 }
